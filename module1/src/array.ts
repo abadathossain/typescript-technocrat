872582876602f27372c0347e6829a18d24201152
Abadat Hossain <abadat26@gmail.com>
2024-10-31 08:57:36 +0600

    object literal done

---
+++ module1/src/array.ts
@@ -0,0 +1,11 @@
+let friends:string[]=['a','b','c'];
+
+let age:number[]=[1,2,3,4,5];
+age.push(8)
+console.log(age)
+
+
+// tuple
+let ageName:[number,string]=[4,'A']
+// ageName.push(8,'Y')
+console.log(ageName)
---
+++ module1/src/object.ts
@@ -0,0 +1,12 @@
+const user:{
+    readonly fName:string;
+    mName:string;
+    lName:string;
+    isMarried:boolean;
+}={
+    fName:'A',
+    mName:'B',
+    lName:'C',
+    isMarried:true,
+}
+// user.fName='B' can not change this
