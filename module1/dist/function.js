29dd3000943cebea39c1921479bc3ffcfcf5fec7
Abadat Hossain <abadat26@gmail.com>
2024-10-31 09:52:44 +0600

    ts map is done

--- module1/dist/function.js
+++ module1/dist/function.js
@@ -4,3 +4,10 @@ const addArrow = (num1, num2) => {
 };
 const result = addArrow(2, 3);
 console.log(result);
+const poorUser = {
+    name: 'Abadat',
+    balance: 0,
+    addBalance(balance) {
+        return this.balance + balance;
+    }
+};
---
+++ module1/dist/map.js
@@ -0,0 +1,4 @@
+"use strict";
+const arr = [1, 2, 3, 4];
+const newArr = arr.map((elem) => elem * elem);
+console.log(newArr);
