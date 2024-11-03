f8490f8713a81a4dc3c9b1e163bfdb290b50e513
Abadat Hossain <abadat26@gmail.com>
2024-11-03 21:59:56 +0600

    class is ready

--- module2/src/genericInterface.ts
+++ module2/src/genericInterface.ts
@@ -1,27 +1,29 @@
 {
-    type GenericArray<T>=Array<T>;
-    type User={
-        name:string;
-        age:number
-    }
+// 
+type GenericArray<T>=Array<T>;
+type User={
+    name:string;
+    age:number
+}
 
-    interface User1{
-        name:string;
-        age:number
-    }
-    
+interface User1{
+    name:string;
+    age:number
+}
+
 const user:GenericArray<User>=[
-    {
-        name:'a',
-        age:25
-    },
-    {
-        name:'b',
-        age:35
-    },
-    {
-        name:'c',
-        age:20
-    }
-]
+{
+    name:'a',
+    age:25
+},
+{
+    name:'b',
+    age:35
+},
+{
+    name:'c',
+    age:20
+}
+]  
+// 
 }
